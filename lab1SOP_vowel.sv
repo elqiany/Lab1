3f212307df8b8f70a9708858b36c185a7e96c8c9
Elaine Qian <eqian@ece005.ece.local.cmu.edu>
2026-02-03 10:08:26 -0500

    fixed vowels

--- lab1SOP_vowel.sv
+++ lab1SOP_vowel.sv
@@ -15,20 +15,21 @@ module lab1SOP_vowel (
     //A'C'D'EF'
     logic na1, na2, na3;
     logic na4, na5, na6;
+    logic na42, na52, na62;
 
     //BCD'EF'
     nand (na4, nA, nC, nD, E);
-    nand (na4, na4, na4);
-    nand (na1, na4, nF);
+    nand (na42, na4, na4);
+    nand (na1, na42, nF);
 
     //AB'D'E'F
     nand (na5, B, C, nD, E);
-    nand (na5, na5, na5);
-    nand (na2, na5, nF);
+    nand (na52, na5, na5);
+    nand (na2, na52, nF);
 
     nand (na6, A, nB, nD, nE);
-    nand (na6, na6, na6);
-    nand (na3, na6, F);
+    nand (na62, na6, na6);
+    nand (na3, na62, F);
 
     nand (vowel, na1, na2, na3);
 
