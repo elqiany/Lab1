c26c85ca6d53f348c910b65b150d412b002769ad
Elaine Qian <eqian@ece005.ece.local.cmu.edu>
2026-02-03 09:59:35 -0500

    fixed valids

--- lab1SOP_valid.sv
+++ lab1SOP_valid.sv
@@ -29,10 +29,11 @@ module lab1SOP_valid (
 
     //A'CD'E'F
     logic na12;
+    logic na122;
 
     nand (na12, nA, C, nD, nE);
-    nand (na12, na12, na12);
-    nand (na5, na12, F);
+    nand (na122, na12, na12);
+    nand (na5, na122, F);
 
     //AB'D'E'
     nand (na6, A, nB, nD, nE);
@@ -42,32 +43,37 @@ module lab1SOP_valid (
 
     //A'B'CDF'
     logic na13;
+    logic na132;
+
     nand (na13, nA, nB, C, D);
-    nand (na13, na13, na13);
-    nand (na8, na13, nF);
+    nand (na132, na13, na13);
+    nand (na8, na132, nF);
 
     //B'C'D'E
     nand (na9, nB, nC, nD, E);
 
     //A'BC'EF'
     logic na14;
+    logic na142;
+
     nand (na14, nA, B, nC, E);
-    nand (na14, na14, na14);
-    nand (na10, na14, nF);
+    nand (na142, na14, na14);
+    nand (na10, na142, nF);
 
     //B'C'E'F'
     nand (na11, nB, nC, nE, nF);
 
     logic na15, na16, na17;
+    logic na152, na162, na172;
 
     nand (na15, na1, na2, na3, na4);
     nand (na16, na5, na6, na7, na8);
     nand (na17, na9, na10, na11);
 
-    nand (na15, na15, na15);
-    nand (na16, na16, na16);
-    nand (na17, na17, na17);
+    nand (na152, na15, na15);
+    nand (na162, na16, na16);
+    nand (na172, na17, na17);
 
-    nand (valid, na15, na16, na17);
+    nand (valid, na152, na162, na172);
 
 endmodule : lab1SOP_valid
